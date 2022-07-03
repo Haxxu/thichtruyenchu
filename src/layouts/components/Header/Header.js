17d4e9109da75b64a2d404aa08733ac6f9df6117
Haxu <nguyenngocminh190901@gmail.com>
2022-07-03 13:13:50 +0700

    test cach su dung HeadlessTippy cho phan nav menu

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,6 +1,8 @@
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import { AiOutlineMenu } from 'react-icons/ai';
+import Tippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
 
 import config from '~/config';
 import images from '~/assets/images';
@@ -18,13 +20,27 @@ function Header() {
                     <Link to={config.routes.home} className={cx('logo-link')}>
                         <Image src={images.logo} />
                     </Link>
-                    <div className='nav-menu'>
-                        <AiOutlineMenu size={20} />
-                        <span>Thể loại</span>
+                    <div>
+                        <Tippy
+                            interactive
+                            placement='bottom'
+                            render={(attrs) => (
+                                <div className={cx('menu-items')} tabIndex='-1' {...attrs}>
+                                    <div>
+                                        <h3>Cay the nho</h3>
+                                    </div>
+                                </div>
+                            )}
+                        >
+                            <Link to={config.routes.search} className={cx('nav-menu')}>
+                                <AiOutlineMenu size={20} />
+                                <span>Thể loại</span>
+                            </Link>
+                        </Tippy>
                     </div>
-                    <div className='nav-menu'>
+                    <Link to={config.routes.ranking} className={cx('nav-menu')}>
                         <span>Bảng xếp hạng</span>
-                    </div>
+                    </Link>
                 </div>
                 <Search />
                 <div className={cx('actions')}>
