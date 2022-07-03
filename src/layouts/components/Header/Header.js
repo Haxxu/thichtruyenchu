ba69bed559465e8d6d2a7c9127ba7f3790fe192c
Haxu <nguyenngocminh190901@gmail.com>
2022-07-03 17:40:55 +0700

    Hoàn thiện phần nav-menu trên header

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,16 +1,96 @@
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import { AiOutlineMenu } from 'react-icons/ai';
-import Tippy from '@tippyjs/react/headless';
+import HeadlessTippy from '@tippyjs/react/headless';
+import { Container, Row, Col } from 'react-grid-system';
 import 'tippy.js/dist/tippy.css';
 
 import config from '~/config';
 import images from '~/assets/images';
 import Search from '~/layouts/components/Search';
 import Image from '~/components/Image';
+import { PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 
 const cx = classNames.bind(styles);
+
+const genresMenu = [
+    {
+        title: 'Tất Cả',
+        to: '/search',
+    },
+    {
+        title: 'Huyền Huyễn',
+        to: '/search',
+    },
+    {
+        title: 'Võng Du',
+        to: '/search',
+    },
+    {
+        title: 'Đồng Nhân',
+        to: '/search',
+    },
+    {
+        title: 'Cạnh Kỹ',
+        to: '/search',
+    },
+    {
+        title: 'Kiếm Hiệp',
+        to: '/search',
+    },
+    {
+        title: 'Tiên Hiệp',
+        to: '/search',
+    },
+    {
+        title: 'Khoa Huyễn',
+        to: '/search',
+    },
+    {
+        title: 'Đô Thị',
+        to: '/search',
+    },
+    {
+        title: 'Dã Sử',
+        to: '/search',
+    },
+    {
+        title: 'Huyền Nghi',
+        to: '/search',
+    },
+    {
+        title: 'Kỳ Ảo',
+        to: '/search',
+    },
+];
+
+const rankingMenu = [
+    {
+        title: 'Thịnh Hành',
+        to: '/ranking',
+    },
+    {
+        title: 'Đọc Nhiều',
+        to: '/ranking',
+    },
+    {
+        title: 'Tặng thưởng',
+        to: '/ranking',
+    },
+    {
+        title: 'Đề Cử',
+        to: '/ranking',
+    },
+    {
+        title: 'Yêu thích',
+        to: '/ranking',
+    },
+    {
+        title: 'Thảo Luận',
+        to: '/ranking',
+    },
+];
 
 function Header() {
     return (
@@ -21,26 +101,58 @@ function Header() {
                         <Image src={images.logo} />
                     </Link>
                     <div>
-                        <Tippy
+                        <HeadlessTippy
                             interactive
-                            placement='bottom'
+                            placement='bottom-start'
+                            arrow
                             render={(attrs) => (
-                                <div className={cx('menu-items')} tabIndex='-1' {...attrs}>
-                                    <div>
-                                        <h3>Cay the nho</h3>
-                                    </div>
-                                </div>
+                                <PopperWrapper className={cx('menu-items')} tabIndex='-1' {...attrs}>
+                                    <Container>
+                                        <Row>
+                                            {genresMenu.map((genre, index) => (
+                                                <Col sm={6}>
+                                                    <Link to={genre.to} className={cx('memu-item')} key={index}>
+                                                        {genre.title}
+                                                    </Link>
+                                                </Col>
+                                            ))}
+                                        </Row>
+                                    </Container>
+                                </PopperWrapper>
                             )}
                         >
                             <Link to={config.routes.search} className={cx('nav-menu')}>
                                 <AiOutlineMenu size={20} />
                                 <span>Thể loại</span>
                             </Link>
-                        </Tippy>
+                        </HeadlessTippy>
                     </div>
-                    <Link to={config.routes.ranking} className={cx('nav-menu')}>
-                        <span>Bảng xếp hạng</span>
-                    </Link>
+                    <div>
+                        <HeadlessTippy
+                            interactive
+                            placement='bottom-start'
+                            arrow
+                            render={(attrs) => (
+                                <PopperWrapper className={cx('menu-items')} tabIndex='-1' {...attrs}>
+                                    <Container>
+                                        <Row>
+                                            {rankingMenu.map((item, index) => (
+                                                <Col sm={6}>
+                                                    <Link to={item.to} className={cx('memu-item')} key={index}>
+                                                        {item.title}
+                                                    </Link>
+                                                </Col>
+                                            ))}
+                                        </Row>
+                                    </Container>
+                                </PopperWrapper>
+                            )}
+                        >
+                            <Link to={config.routes.ranking} className={cx('nav-menu')}>
+                                <span>Bảng xếp hạng</span>
+                            </Link>
+                        </HeadlessTippy>
+                    </div>
                 </div>
                 <Search />
                 <div className={cx('actions')}>
